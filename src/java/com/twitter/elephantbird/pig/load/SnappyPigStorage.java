94b0062fcf80767b8ddaa456a49d6d9646f54cbb
chaitanya sharma <gopi.daiict@gmail.com>
2011-04-30 23:41:56 -0400

    * updated the build.xml to support thrift 0.7-dev * added a dummpy snappy-pig-storage loader.

---
+++ src/java/com/twitter/elephantbird/pig/load/SnappyPigStorage.java
@@ -0,0 +1,43 @@
+package com.twitter.elephantbird.pig.load;
+
+import java.io.IOException;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.InputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.pig.builtin.PigStorage;
+
+import com.hadoop.compression.lzo.LzopCodec;
+import com.twitter.elephantbird.mapreduce.input.LzoTextInputFormat;
+
+/**
+ * 
+ * A Snappy Loader based on PigStorage Advantages of using this class is that we
+ * get all of the improvements that PigStorage have and might get for free.
+ * 
+ */
+public class SnappyPigStorage extends PigStorage {
+
+	public SnappyPigStorage() {
+		super();
+	}
+
+	public SnappyPigStorage(String delimiter) {
+		super(delimiter);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public InputFormat getInputFormat() {
+		return new LzoTextInputFormat();
+	}
+	
+	@Override
+	public void setStoreLocation(String location, Job job) throws IOException {
+		job.getConfiguration().set("mapred.textoutputformat.separator", "");
+		FileOutputFormat.setOutputPath(job, new Path(location));
+		FileOutputFormat.setCompressOutput(job, true);
+		FileOutputFormat.setOutputCompressorClass(job, LzopCodec.class);
+	}
+}
